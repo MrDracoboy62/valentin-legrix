22c63a8ee6d0c1344ce818d227f1443f9ee3c30a
Valentin L. (MrDracoboy62) <51105090+MrDracoboy62@users.noreply.github.com>
2022-06-06 20:21:28 +0200

    Add files via upload

---
+++ Projet Perso/script.js
@@ -0,0 +1,66 @@
+function Lien() {
+	i = document.solution.Liste.selectedIndex;
+	if (i == 0) return;
+	url = document.solution.Liste.options[i].value;
+	parent.location.href = url;
+}
+function Lien() {
+	i = document.installation.Liste.selectedIndex;
+	if (i == 0) return;
+	url = document.installation.Liste.options[i].value;
+	parent.location.href = url;
+}
+function Lien() {
+	i = document.pokedex.Liste.selectedIndex;
+	if (i == 0) return;
+	url = document.pokedex.Liste.options[i].value;
+	parent.location.href = url;
+}
+function Lien() {
+	i = document.carte.Liste.selectedIndex;
+	if (i == 0) return;
+	url = document.carte.Liste.options[i].value;
+	parent.location.href = url;
+}
+function Lien() {
+	i = document.sac.Liste.selectedIndex;
+	if (i == 0) return;
+	url = document.sac.Liste.options[i].value;
+	parent.location.href = url;
+}
+function Lien() {
+	i = document.personnage.Liste.selectedIndex;
+	if (i == 0) return;
+	url = document.personnage.Liste.options[i].value;
+	parent.location.href = url;
+}
+function Lien() {
+	i = document.cadeau.Liste.selectedIndex;
+	if (i == 0) return;
+	url = document.cadeau.Liste.options[i].value;
+	parent.location.href = url;
+}
+function Lien() {
+	i = document.legendaire.Liste.selectedIndex;
+	if (i == 0) return;
+	url = document.legendaire.Liste.options[i].value;
+	parent.location.href = url;
+}
+function Lien() {
+	i = document.mega.Liste.selectedIndex;
+	if (i == 0) return;
+	url = document.mega.Liste.options[i].value;
+	parent.location.href = url;
+}
+function Lien() {
+	i = document.outil.Liste.selectedIndex;
+	if (i == 0) return;
+	url = document.outil.Liste.options[i].value;
+	parent.location.href = url;
+}
+function Lien() {
+	i = document.musique.Liste.selectedIndex;
+	if (i == 0) return;
+	url = document.musique.Liste.options[i].value;
+	parent.location.href = url;
+}
